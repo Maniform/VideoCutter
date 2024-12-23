be6746863095dd21c5c5ba2553a3808777eb012c
Maniform <maniform.development@gmail.com>
2024-12-24 00:41:21 +0100

    First commit

---
+++ MainWindow.cpp
@@ -0,0 +1,191 @@
+#include "MainWindow.h"
+#include "./ui_MainWindow.h"
+
+#include <QFileDialog>
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+	: QMainWindow(parent)
+	, ui(new Ui::MainWindow)
+	, settings("Manicorp", "VideoCutter", this)
+	, videoDuration(0)
+{
+	ui->setupUi(this);
+
+	ui->videoInLineEdit->setText(settings.value("VideoInLineEdit").toString());
+	ui->videoOutLineEdit->setText(settings.value("VideoOutLineEdit").toString());
+	ui->startHourSpinBox->setValue(settings.value("StartHour").toInt());
+	ui->startMinSpinBox->setValue(settings.value("StartMin").toInt());
+	ui->startSecSpinBox->setValue(settings.value("StartSec").toInt());
+	ui->endHourSpinBox->setValue(settings.value("EndHour").toInt());
+	ui->endMinSpinBox->setValue(settings.value("EndMin").toInt());
+	ui->endSecSpinBox->setValue(settings.value("EndSec").toInt());
+
+	ui->progressBar->hide();
+	ui->stopPushButton->hide();
+
+	connect(&ffmpeg, &QProcess::readyReadStandardOutput, this, &MainWindow::onReadyRead);
+	connect(&ffmpeg, &QProcess::readyReadStandardError, this, &MainWindow::onReadyRead);
+	connect(&ffmpeg, &QProcess::finished, this, &MainWindow::onCutVideoProcessFinished);
+}
+
+MainWindow::~MainWindow()
+{
+	disconnect(&ffmpeg, &QProcess::readyReadStandardOutput, this, &MainWindow::onReadyRead);
+	disconnect(&ffmpeg, &QProcess::readyReadStandardError, this, &MainWindow::onReadyRead);
+	disconnect(&ffmpeg, &QProcess::finished, this, &MainWindow::onCutVideoProcessFinished);
+
+	ffmpeg.kill();
+
+	settings.setValue("VideoInLineEdit", ui->videoInLineEdit->text());
+	settings.setValue("VideoOutLineEdit", ui->videoOutLineEdit->text());
+	settings.setValue("StartHour", ui->startHourSpinBox->value());
+	settings.setValue("StartMin", ui->startMinSpinBox->value());
+	settings.setValue("StartSec", ui->startSecSpinBox->value());
+	settings.setValue("EndHour", ui->endHourSpinBox->value());
+	settings.setValue("EndMin", ui->endMinSpinBox->value());
+	settings.setValue("EndSec", ui->endSecSpinBox->value());
+	
+	delete ui;
+}
+
+int MainWindow::stringToSec(QString text)
+{
+	const QStringList values = text.split(":");
+	const int sec = values[0].toInt() * 3600 + values[1].toInt() * 60 + values[2].toInt();
+	return sec;
+}
+
+QString MainWindow::getEndTime()
+{
+	const int startSec = ui->startHourSpinBox->value() * 3600 + ui->startMinSpinBox->value() * 60 + ui->startSecSpinBox->value();
+	const int endSec = ui->endHourSpinBox->value() * 3600 + ui->endMinSpinBox->value() * 60 + ui->endSecSpinBox->value();
+	int duration = endSec - startSec;
+
+	const int hour = duration / 3600;
+	QString endTime = QString::number(hour) + ":";
+	duration -= hour * 3600;
+	const int min = duration / 60;
+	endTime += QString::number(min) + ":";
+	duration -= min * 60;
+	endTime += QString::number(duration);
+
+	return endTime;
+}
+
+void MainWindow::onVideoInPushButtonClicked()
+{
+	const QString folder = ui->videoInLineEdit->text().isEmpty() ? QDir::homePath() : ui->videoInLineEdit->text();
+	const QString fileName = QFileDialog::getOpenFileName(this, tr("Ouvrir le fichier vidéo"), folder, "*.mkv *.mp4;;*.*");
+	if (!fileName.isEmpty())
+	{
+		ui->videoInLineEdit->setText(fileName);
+	}
+}
+
+void MainWindow::onVideoOutPushButtonClicked()
+{
+	const QString folder = ui->videoOutLineEdit->text().isEmpty() ? QDir::homePath() : ui->videoOutLineEdit->text();
+	const QString fileName = QFileDialog::getSaveFileName(this, tr("Ouvrir le fichier vidéo"), folder, "*.mkv;;*.mp4;;*.*");
+	if (!fileName.isEmpty())
+	{
+		ui->videoOutLineEdit->setText(fileName);
+	}
+}
+
+void MainWindow::onCutVideoPushButtonClicked()
+{
+	if (ui->videoInLineEdit->text() == ui->videoOutLineEdit->text())
+	{
+		QMessageBox::warning(this, tr("Problème"), tr("Le fichier d'entrée ne peux pas être le même que le fichier de sortie !"));
+		return;
+	}
+
+	const QString ffmpegExe = "ffmpeg.exe";
+	const QString startTime = QString::number(ui->startHourSpinBox->value()) + ":" + QString::number(ui->startMinSpinBox->value()) + ":" + QString::number(ui->startSecSpinBox->value());
+	const QString endTime = getEndTime();
+	videoDuration = stringToSec(endTime);
+	QString vcodec = "h264";
+	QStringList startTimeCommand;
+	if (stringToSec(startTime) == 0)
+	{
+		vcodec = "copy";
+	}
+	else
+	{
+		QApplication::beep();
+		QMessageBox message(tr("Attention"), tr("Couper la vidéo au début force le réencodage et ce processus peut-être très long. Continuer quand même ?"), QMessageBox::Question, QMessageBox::Yes, QMessageBox::No, QMessageBox::NoButton, this);
+		message.setButtonText(QMessageBox::Yes, tr("Oui"));
+		message.setButtonText(QMessageBox::No, tr("Non"));
+		message.exec();
+		if (message.result() == QMessageBox::No)
+		{
+			return;
+		}
+		startTimeCommand << "-ss" << startTime;
+	}
+	QStringList ffmpegArguments;
+	ffmpegArguments
+	<< "-y"
+	<< "-i" << ui->videoInLineEdit->text()
+	<< "-vcodec" << vcodec
+	<< "-acodec" << "copy"
+	<< startTimeCommand
+	<< "-t" << endTime
+	<< ui->videoOutLineEdit->text();
+
+	ffmpeg.start(ffmpegExe, ffmpegArguments);
+	ui->progressBar->show();
+	ui->stopPushButton->show();
+	ui->cutvideoPushButton->hide();
+}
+
+void MainWindow::onStopCutPushButtonClicked()
+{
+	ffmpeg.kill();
+}
+
+void MainWindow::onReadyRead()
+{
+	QString output = ffmpeg.readAllStandardOutput();
+	QString error = ffmpeg.readAllStandardError();
+
+	QString text = output.contains("time=") ? output : error;
+	if (text.contains("time="))
+	{
+		const QString timeText = text.split("time=").last().split(" ").first().split(".").first();
+		const float sec = stringToSec(timeText);
+		if (sec > 0)
+		{
+			float progress = sec / videoDuration * 100.f;
+			ui->progressBar->setValue(progress);
+		}
+	}
+}
+
+void MainWindow::onCutVideoProcessFinished(int exitCode, QProcess::ExitStatus exitStatus)
+{
+	ui->stopPushButton->hide();
+	ui->progressBar->hide();
+	ui->progressBar->setValue(0);
+	ui->cutvideoPushButton->show();
+	const QString output = ffmpeg.readAllStandardOutput();
+	const QString error = ffmpeg.readAllStandardError();
+	if (ui->actionLog->isChecked())
+	{
+		QFile outputFile(QDir::homePath() + "/output.txt");
+		if (outputFile.open(QFile::WriteOnly))
+		{
+			QTextStream outputStream(&outputFile);
+			outputStream << output;
+		}
+
+		QFile errorFile(QDir::homePath() + "/error.txt");
+		if (errorFile.open(QFile::WriteOnly))
+		{
+			QTextStream errorStream(&errorFile);
+			errorStream << error;
+		}
+	}
+	QMessageBox::information(this, tr("Travail terminé"), tr("La vidéo a été coupée."));
+}
---
+++ MainWindow.h
@@ -0,0 +1,42 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QProcess>
+#include <QSettings>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    int stringToSec(QString text);
+    QString getEndTime();
+
+private slots:
+    void onVideoInPushButtonClicked();
+    void onVideoOutPushButtonClicked();
+    void onCutVideoPushButtonClicked();
+    void onStopCutPushButtonClicked();
+
+    void onReadyRead();
+    void onCutVideoProcessFinished(int exitCode, QProcess::ExitStatus exitStatus);
+    
+private:
+    Ui::MainWindow *ui;
+
+    QSettings settings;
+    QProcess ffmpeg;
+    int videoDuration;
+};
+#endif // MAINWINDOW_H
